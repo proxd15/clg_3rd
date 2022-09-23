fe487b4588844ee5e8a9ee716ebab2c9997cb1c1
proxd15 <94702040+proxd15@users.noreply.github.com>
2022-09-23 21:32:23 +0530

    Add files via upload

---
+++ python/ques.py
@@ -0,0 +1,18 @@
+# a = int(input("Enter your age"))
+# b = int(input("Enter your height in cm"))
+
+# print(a)
+# print(b)
+
+c = float(input("Enter first number "))
+d = float(input("Enter second number "))
+
+add = c + d;
+sub = c - d;
+mul = c * d;
+div = c / d;
+
+print(add)
+print(sub)
+print(mul)
+print(div)
